b97d441109d9de9140a23513b09f9ee355ae0741
hr1shabh <hrishabhpalsra@gmail.com>
2025-05-01 22:57:58 +0530

    added a test database and configure dependencies

---
+++ TodoApp/test/test_todos.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from ..database import Base
+from ..main import app
+from ..routers.todos import get_db, get_current_user
+from fastapi.testclient import TestClient
+from fastapi import status
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./testdb.db'
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={'check_same_thread': False},
+    poolclass=StaticPool
+)
+
+TestingSessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def ovveride_get_current_user():
+    return {'username': 'hrishabh_test', 'id': 1, 'user_role': 'admin'}
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = ovveride_get_current_user
+
+client = TestClient(app)
+
+def test_read_all_authenticated():
+    response = client.get('/')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
+
+
